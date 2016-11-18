ae9c5fd79230955b59182045a4db3a785376a44b
lichangjie <lichangjie@innobuddy.com>
2016-11-18 18:17:38 +0800

    rnn draft

---
+++ rnn/rnn.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
